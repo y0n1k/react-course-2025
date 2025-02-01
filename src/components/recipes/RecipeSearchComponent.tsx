fdfead6fe8fe04a36a8dae83d0f4c48302ceb00b
Пекарський Ян bc2024-4 <yan.pekarski@gmail.com>
2025-02-01 20:56:46 +0200

    joi and hookform/resolvers installed

--- src/components/recipes/RecipeSearchComponent.tsx
+++ src/components/recipes/RecipeSearchComponent.tsx
@@ -1,36 +1,20 @@
-import {FormEvent, useState} from "react";
+import {useForm} from "react-hook-form";
 
 type IFormProps = {
     name: string
 }
 
 const RecipeSearchComponent = () => {
-    const [formState, setFormState] = useState<IFormProps>({
-        name: ''
-    })
-
-
-    function handler(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        console.log(e);
-        // const form = e.target as HTMLFormElement;
-        // console.log(form.name.value)
-    }
-
-    function changeHandler(e:FormEvent<HTMLInputElement>) {
-e.preventDefault();
-const input = e.target as HTMLInputElement;
-console.log(input.value)
-setFormState({...formState, [input.name]: input.value});
-    }
-
+    const {handleSubmit, register} = useForm<IFormProps>()
 
     return (
         <div>
             <hr/>
-            <form onSubmit={handler}>
+            <form onSubmit={handleSubmit((formDataProps: IFormProps) => {
+                console.log(formDataProps);
+            })}>
                 <label>Пошук</label>
-                <input type={"text"} name={"name"} placeholder={"Введіть назва рецепта"} onChange={changeHandler}/>
+                <input type={"text"} {...register('name')} placeholder={"Введіть назва рецепта"}/>
                 <button>Надіслати</button>
             </form>
             <hr/>
